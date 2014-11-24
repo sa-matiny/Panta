122f7eaecbd7c5e645ad93f971930e2eaf065e68
sa-matiny <sa_matiny@yahoo.com>
2014-11-24 23:59:14 +0330

    fiiiiiiiiiiiiiiiiiiiiiiiiiiixxxx

--- app/src/main/java/com/iust/panta/FirstPage.java
+++ app/src/main/java/com/iust/panta/FirstPage.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 
-public class Welcome extends Activity {
+public class FirstPage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
