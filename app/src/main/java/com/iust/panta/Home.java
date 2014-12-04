08280c2863acad4eb3ff5321a00cdde71787a01d
sa-matiny <sa_matiny@yahoo.com>
2014-12-05 01:11:17 +0330

    fixes for home :(((

--- app/src/main/java/com/iust/panta/Home.java
+++ app/src/main/java/com/iust/panta/Home.java
@@ -9,206 +9,62 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
 
-import com.iust.panta.Expands.DataHelper;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import org.apache.http.Header;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 
 public class Home extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    private ArrayList<String> listProjectNames;
-    private ArrayList<String> listProjectIDs;
-    private ArrayList<ArrayList<String>> listTaskNamesforEachProjects;
-    private ArrayList<ArrayList<String>> listTasksIDs;
-    private String name;
-    private Bundle bundle_data;
-    private String userName;
+
+    SqliteController controller = new SqliteController(this);
     private NavigationDrawerFragment mNavigationDrawerFragment;
-    private CharSequence mTitle;
-    //private class act;
+    private Bundle msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        listProjectNames = new ArrayList<String>();
-        listProjectIDs=new ArrayList<String>();
-        listTaskNamesforEachProjects = new ArrayList<ArrayList<String>>();
-        listTasksIDs=new ArrayList<ArrayList<String>>();
-        //      super.onCreate();
-        bundle_data = new Bundle();
+        msg = new Bundle();
+        String userName;
 
-        mTitle = getTitle();
+        try {
+            userName = controller.getMe().getString("username");
+            msg.putString("username", userName);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         super.onCreate(savedInstanceState);
-        try {
-
-
-            Intent intent = getIntent();
-            // bundle_data=intent.getExtras();
-            JSONObject info = new JSONObject(intent.getExtras().getString("user_info"));
-            setTitle(info.getString("name"));
-            userName = new String(info.getString("username"));
-            Log.d("username_profile", userName);
-        } catch (JSONException info) {
-
-            Log.d("trye gonde", "tryGonde");
-        }
-        RequestParams params = new RequestParams();
-        params.put("username", userName);
-        // params.put("password", password);
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.post("http://104.236.33.128:8800/view_profile/", params, new AsyncHttpResponseHandler() {
-
-            @Override
-            public void onStart()
-            {
-                System.out.println("profileStart");
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                try {
-
-                    Log.d("successs","success");
-                    //        Log.d("try", intent.getExtras().getString("projects"));
-                    //      JSONArray Projects_obj = new JSONArray(intent.getExtras().getString("projects"));
-                    //  Log.d("profile_respone",new String(response));
-
-                    JSONObject jObject = new JSONObject(new String(response));
-                    JSONArray Projects_obj = jObject.getJSONArray("projects");
-
-                    Log.d("projectsjsonArray", Projects_obj.toString());
-
-                    //        bundle_data =new Bundle(new String(response));
-                    for (int i = 0; i < Projects_obj.length(); i++) {
-
-                        Log.d("jsonArrayItems", Projects_obj.get(i).toString());
-
-                        JSONObject eachProjects = Projects_obj.getJSONObject(i);
-
-                        Log.d("projectName :", eachProjects.getString("projectName"));
-                        listProjectNames.add(eachProjects.getString("projectName"));
-                        Log.d("projectID :", eachProjects.getString("projectID"));
-                        listProjectIDs.add(eachProjects.getString("projectID"));
-                        JSONArray jsonTasks = eachProjects.getJSONArray("tasks");
-                        ArrayList<String> eachTask = new ArrayList<String>();
-                        ArrayList<String> eachTaskID= new ArrayList<String>();
-
-
-                        for (int j = 0; j < jsonTasks.length(); j++) {
-
-                            JSONObject taskInformation = jsonTasks.getJSONObject(j);
-                            Log.d("tasks :" + j, taskInformation.getString("taskName"));
-                            Log.d("tasks :" + j, taskInformation.getString("taskID"));
-                            eachTask.add(taskInformation.getString("taskName"));
-                            eachTaskID.add(taskInformation.getString("taskID"));
-
-                        }
-                        Log.d("end of for 2","end of for 2");
-                        listTaskNamesforEachProjects.add(eachTask);
-                        listTasksIDs.add(eachTaskID);
-
-
-                        // listProjectNames.add(temp.getString("projectName"));
-                    }
-                    bundle_data.putSerializable("tasksName", new DataHelper(listTaskNamesforEachProjects));
-                    bundle_data.putStringArrayList("projectsName", listProjectNames);
-                    bundle_data.putStringArrayList("projectsID", listProjectIDs);
-                    bundle_data.putSerializable("tasksID",new DataHelper(listTasksIDs));
-                } catch (JSONException jsone) {
-
-                    //  expGroup = SetStandardGroup(hasData);
-                    jsone.printStackTrace();
-                    Log.d("exception", "exept");
-
-                }
-
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-
-                Log.d("problem", "problem");
-              /*  mProgressView.setVisibility(View.GONE);
-                mButtonView.setVisibility(View.VISIBLE);
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                builder.setCancelable(false);
-                builder.setMessage("خطا! دوباره وارد شوید");
-                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();*/
-            }
-
-            @Override
-            public void onFinish() {
-
-
-
-                setContentView(R.layout.activity_home);
-                mNavigationDrawerFragment = (NavigationDrawerFragment)
-                        getFragmentManager().findFragmentById(R.id.navigation_drawer);
-
-                mNavigationDrawerFragment.setUp(
-                        R.id.navigation_drawer,
-                        (DrawerLayout) findViewById(R.id.drawer_layout));
-
-
-            }
-
-
-        });
+        setContentView(R.layout.activity_home);
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        Log.d("hereeee", "hereee");
 
         Fragment objfrag = null;
-        Bundle bundle = new Bundle();
 
         switch (position) {
             case 0:
-
                 objfrag = new HomeProfileFragment();
-                bundle.putBundle("Projects", bundle_data);
-                objfrag.setArguments(bundle);
-
-
+                objfrag.setArguments(msg);
                 break;
             case 1:
-                bundle.putString("username", userName);
                 objfrag = new HomeAddProjectFragment();
-                objfrag.setArguments(bundle);
+                objfrag.setArguments(msg);
                 break;
             case 2:
                 objfrag = new HomeSettingFragment();
                 break;
             default:
                 objfrag = new HomeProfileFragment();
-                bundle.putBundle("Projects", bundle_data);
-                objfrag.setArguments(bundle);
-
-
+                objfrag.setArguments(msg);
                 break;
 
         }
@@ -219,27 +75,12 @@ public class Home extends Activity
                 .commit();
     }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = name;
-            case 2:
-                mTitle = getString(R.string.Home_section);
-                break;
-            case 3:
-                mTitle = getString(R.string.Setting_section);
-                break;
-            case 4:
-                mTitle = getString(R.string.add_section);
-                break;
-        }
-    }
 
     public void restoreActionBar() {
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+        actionBar.setTitle(R.string.Home_section);
     }
 
 
@@ -255,8 +96,30 @@ public class Home extends Activity
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("می خواهید از برنامه خارج شوید؟");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                controller.deleteMe();
+
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                finish();
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+
+    }
 }
-
-//testing
-
-
--- app/src/main/java/com/iust/panta/HomeProfileFragment.java
+++ app/src/main/java/com/iust/panta/HomeProfileFragment.java
@@ -1,26 +1,31 @@
 package com.iust.panta;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.util.Log;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import com.iust.panta.Expand.adapter.ExpandListViewAdapter;
-import com.iust.panta.Expands.DataHelper;
 import com.iust.panta.Expands.ExpandChildList;
 import com.iust.panta.Expands.ExpandGroupList;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by Rayehe on 11/23/2014.
@@ -29,31 +34,26 @@ import java.util.HashMap;
 
 
 public class HomeProfileFragment extends Fragment {
-
-    private Bundle bundle;
+    public ExpandableListView Explist;
+    public boolean hasData;
     private ExpandListViewAdapter expandAdapter;
     private ArrayList<ExpandGroupList> expGroup;
-    public ExpandableListView Explist;
-
     private ArrayList<String> listProjectsName;
     private ArrayList<String> listProjectsID;
     private ArrayList<ArrayList<String>> listEachProjectsTask;
     private ArrayList<ArrayList<String>> listEachProjectsTaskID;
+    private View rootView;
 
-    public boolean hasData;
-
-    public HashMap<String, String> project_name;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            saveInstanceState) {
 
-        bundle = new Bundle();
-        Bundle recievie = new Bundle();
+        rootView = inflater.inflate(R.layout.fragment_home_profile, container, false);
+        Explist = (ExpandableListView) rootView.findViewById(R.id.Final_list);
 
-        recievie = this.getArguments();
-        bundle = recievie.getBundle("Projects");
-        hasData = true;
+        ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setTitle(R.string.Home_section);
 
         listEachProjectsTask = new ArrayList<ArrayList<String>>();
         listEachProjectsTaskID = new ArrayList<ArrayList<String>>();
@@ -62,56 +62,106 @@ public class HomeProfileFragment extends Fragment {
         listProjectsID= new ArrayList<String >();
 
 
-        listProjectsName = (bundle.getStringArrayList("projectsName"));
-        listProjectsID=(bundle.getStringArrayList("projectsID"));
-        DataHelper d=(DataHelper)bundle.getSerializable("tasksName");
-        listEachProjectsTask=d.getList();
-        d=(DataHelper)bundle.getSerializable("tasksID");
-        listEachProjectsTaskID=d.getList();
+        Bundle msg = getArguments();
+        String userName = msg.getString("username");
+        Log.d("username_profile", userName);
 
-        // Log.d("seriazable",(listEachProjectsTask.toString()));
-        //  listEachProjectsTask=new ArrayList<ArrayList<String>>().get;
+        RequestParams params = new RequestParams();
+        params.put("username", userName);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post("http://104.236.33.128:8800/view_profile/", params, new AsyncHttpResponseHandler() {
 
-
-        //  JSONArray objProjectInfo = new JSONArray(bundleProjectInfo.getString("projects"));
-        if (listProjectsName.isEmpty()) {
-            hasData = false;
-        }
-
-
-        expGroup = SetStandardGroup(hasData);
-
-    }
-
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
-            saveInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home_profile, container, false);
-
-
-        this.Explist = (ExpandableListView) rootView.findViewById(R.id.Final_list);
-
-
-        expandAdapter = new ExpandListViewAdapter(rootView.getContext(), expGroup);
-
-        Explist.setAdapter(expandAdapter);
-
-        Explist.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
-
-              //
-              //  final String idd =
-               // Log.d("Clicccccked",id);
-                 String idd=listEachProjectsTaskID.get(groupPosition).get(childPosition);
-
-                 Intent intent=new Intent(HomeProfileFragment.this.getActivity(),TaskCard.class);
-                 intent.putExtra("taskID",Integer.parseInt(idd));
-                 startActivity(intent);
-
-                return true;
+            public void onStart() {
+                System.out.println("profileStart");
             }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                try {
+                    JSONObject jObject = new JSONObject(new String(response));
+                    JSONArray Projects_obj = jObject.getJSONArray("projects");
+
+                    Log.d("projectsjsonArray", Projects_obj.toString());
+
+                    for (int i = 0; i < Projects_obj.length(); i++) {
+
+                        Log.d("jsonArrayItems", Projects_obj.get(i).toString());
+
+                        JSONObject eachProjects = Projects_obj.getJSONObject(i);
+
+                        Log.d("projectName :", eachProjects.getString("projectName"));
+                        listProjectsName.add(eachProjects.getString("projectName"));
+                        Log.d("projectID :", eachProjects.getString("projectID"));
+                        listProjectsID.add(eachProjects.getString("projectID"));
+                        JSONArray jsonTasks = eachProjects.getJSONArray("tasks");
+                        ArrayList<String> eachTask = new ArrayList<String>();
+                        ArrayList<String> eachTaskID = new ArrayList<String>();
+
+
+                        for (int j = 0; j < jsonTasks.length(); j++) {
+
+                            JSONObject taskInformation = jsonTasks.getJSONObject(j);
+                            Log.d("tasks :" + j, taskInformation.getString("taskName"));
+                            Log.d("tasks :" + j, taskInformation.getString("taskID"));
+                            eachTask.add(taskInformation.getString("taskName"));
+                            eachTaskID.add(taskInformation.getString("taskID"));
+
+                        }
+                        Log.d("end of for 2", "end of for 2");
+                        listEachProjectsTask.add(eachTask);
+                        listEachProjectsTaskID.add(eachTaskID);
+                    }
+
+                } catch (JSONException jsone) {
+                    jsone.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                System.out.println("Fail");
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setCancelable(false);
+                builder.setMessage("خطا! اتصال به اینترنت با مشکل مواجه است");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+
+            @Override
+            public void onFinish() {
+
+                hasData = true;
+                if (listProjectsName.isEmpty()) {
+                    hasData = false;
+                }
+                expGroup = SetStandardGroup(hasData);
+                expandAdapter = new ExpandListViewAdapter(rootView.getContext(), expGroup);
+
+                Explist.setAdapter(expandAdapter);
+
+                Explist.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+                    @Override
+                    public boolean onChildClick(ExpandableListView parent, View v,
+                                                int groupPosition, int childPosition, long id) {
+                        String idd = listEachProjectsTaskID.get(groupPosition).get(childPosition);
+
+                        Intent intent = new Intent(HomeProfileFragment.this.getActivity(), TaskCard.class);
+                        intent.putExtra("taskID", Integer.parseInt(idd));
+                        startActivity(intent);
+
+                        return true;
+                    }
+                });
+
+            }
+
         });
 
 
@@ -131,61 +181,25 @@ public class HomeProfileFragment extends Fragment {
                 expandGroup.SetName(listProjectsName.get(i));
                 expandGroup.setId(listProjectsID.get(i));
 
-                  if (!listEachProjectsTask.get(i).isEmpty()) {
+                if (!listEachProjectsTask.get(i).isEmpty()) {
 
-                        for (int j = 0; j < listEachProjectsTask.get(i).size(); j++) {
-                            ExpandChildList expandChild = new ExpandChildList();
+                    for (int j = 0; j < listEachProjectsTask.get(i).size(); j++) {
+                        ExpandChildList expandChild = new ExpandChildList();
 
-                            expandChild.setName(listEachProjectsTask.get(i).get(j));
-                            expandChild.setID(listEachProjectsTaskID.get(i).get(j));
-                            expandChild.setTag(null);
-                            expandChildArray.add(expandChild);
-                        }
-                  }
-                /*else
-                  {
-                      //I
-                  }*/
+                        expandChild.setName(listEachProjectsTask.get(i).get(j));
+                        expandChild.setID(listEachProjectsTaskID.get(i).get(j));
+                        expandChild.setTag(null);
+                        expandChildArray.add(expandChild);
+                    }
+                }
                 expandGroup.setItemes(expandChildArray);
                 expandGroupArray.add(expandGroup);
 
             }
         } else {
-
             Toast.makeText(this.getActivity(), "برای شما پروژه ای موجود نمی باشد .", Toast.LENGTH_LONG).show();
-
-
         }
         return expandGroupArray;
     }
 
 }
-
-/**
- * Back button listener.
- * Will close the application if the back button pressed twice.
- */
-
-    /*@Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("می خواهید از برنامه خرج شوید؟");
-        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_HOME);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
-        AlertDialog alert = builder.create();
-        alert.show();
-
-    }
-}*/
-
-
-
